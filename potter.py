ee7275530af99e504560c940907e81ef87cb0edc
Davi Lima <davilima6@gmail.com>
2011-03-31 20:44:32 -0300

    Passando a cesta com um livro (Luiz Eduardo)

--- potter.py
+++ potter.py
@@ -1,11 +1,16 @@
 import unittest
+BOOK_1 = 0
 
 def book_calculator(basket):
-    return 0
+    if basket == None:
+        return 0
+    return 8
 
 class Book_calculator(unittest.TestCase):
     def test_empty_basket(self):
         self.assertEqual(0, book_calculator(None))
+    def test_one_book(self):
+        self.assertEqual(8, book_calculator({ BOOK_1: 1 }))
 
 if __name__ == '__main__':
     unittest.main()
