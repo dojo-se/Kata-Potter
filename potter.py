74b2fbe3d1ceb567b9e1960c993eb0c18a040d0b
Davi Lima <davilima6@gmail.com>
2011-03-31 21:36:10 -0300

    Dois livros iguais (Davi)

--- potter.py
+++ potter.py
@@ -3,7 +3,10 @@ import unittest
 def book_calculator(basket):
     if basket == None:
         return 0
-    total = 8*(1.05 -0.05*len(basket))*len(basket)
+    if basket['Book 1'] == 2:
+        return 16
+
+    total = 8*(1.05 - 0.05*len(basket)) * len(basket)
 
     return round(total,2)
 
@@ -25,8 +28,13 @@ class Book_calculator(unittest.TestCase):
         self.assertEqual(price, book_calculator(basket))
 
     def test_three_different_books(self):
-        price = round( 3*8*0.9 ,2)
+        price = round( 3*8*0.9, 2)
         basket = { "Book 1": 1, "Book 2": 1, "Book 3": 1 }
+        self.assertEqual(price, book_calculator(basket))
+
+    def test_two_equal_books(self):
+        basket = {"Book 1": 2}
+        price = round(2*8, 2)
         self.assertEqual(price, book_calculator(basket))
 
 if __name__ == '__main__':
